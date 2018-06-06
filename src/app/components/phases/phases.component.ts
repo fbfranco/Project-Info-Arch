0e0e49d1331bef8d2c2bfe1f6936e21c7df0736a
Daniel David Alvarez Rivero <dalvarez@info-arch.com>
2018-06-06 14:11:48 -0400

    Cambios Revisados

--- src/app/components/phases/phases.component.ts
+++ src/app/components/phases/phases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-phases',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhasesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+  // openDialog(): void {
+  //   let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+  //     width: '250px',
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log('The dialog was closed');
+  //   });
+
+  //   @Component({
+  //     selector: 'dialog-overview-example-dialog',
+  //     templateUrl: 'dialog-overview-example-dialog.html',
+  //   })
+  //   export class DialogOverviewExampleDialog {
+    
+  //     constructor(
+  //       public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+  //       @Inject(MAT_DIALOG_DATA) public data: any) { }
+    
+  //     onNoClick(): void {
+  //       this.dialogRef.close();
+  //     }
+
 
   ngOnInit() {
   }
